9b619d0e2c99f90a18236a852f1fecef578fe1f7
WanWizard <wanwizard@wanwizard.eu>
2013-08-04 20:30:18 +0200

    moved the Security stuff to the security library

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-# FuelPHP Security library.
+[![Build Status](https://travis-ci.org/fuelphp/security.png?branch=master)](https://travis-ci.org/fuelphp/security)
+
+# FuelPHP Security and Encryption library.
---
+++ src/Fuel/Security/ServicesProvider.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package    Fuel\Security
+ * @version    2.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Security;
+
+use Fuel\Dependency\ServiceProvider;
+use Fuel\Foundation\Application;
+
+/**
+ * ServicesProvider class
+ *
+ * Defines the services published by this namespace to the DiC
+ *
+ * @package  Fuel\Security
+ *
+ * @since  1.0.0
+ */
+class ServicesProvider extends ServiceProvider
+{
+	/**
+	 * @var  array  list of service names provided by this provider
+	 */
+	public $provides = array('security');
+
+	/**
+	 * Service provider definitions
+	 */
+	public function provide()
+	{
+		// \Fuel\Security\Manager
+		$this->register('security', function ($dic, Application $app)
+		{
+			return new Manager($app);
+		});
+	}
+}
